d3c5614c1341d76dd1df2b062a684b4ecfc5cf99
shuvo-220 <shuvo.info220@gmail.com>
2025-06-27 20:21:39 +0600

    create resume

---
+++ backend/controller/resumeController.js
@@ -0,0 +1,9 @@
+const Resume = require('../models/resumeModel');
+
+exports.createResume = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- backend/models/resumeModel.js
+++ backend/models/resumeModel.js
@@ -5,5 +5,80 @@ const resumeSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
         required:true
-    }
-})
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    thumblineLink:{
+        type:String
+    },
+    template:{
+        theme:String,
+        colorPlate:[String]
+    },
+    profileInfo:{
+        profilePreviewUrl:String,
+        fullName:String,
+        designation:String,
+        summery:String
+    },
+    contactInfo:{
+        email:String,
+        phone:String,
+        location:String,
+        linkedin:String,
+        github:String,
+        website:String
+    },
+    //work exp
+    workExperience:[
+        {
+            company:String,
+            role:String,
+            startDate:String,
+            endDate:String,
+            description:String
+        }
+    ],
+    education:[
+        {
+            degree:String,
+            institution:String,
+            startDate:String,
+            endDate:String
+        }
+    ],
+    skills:[
+        {
+            name:String,
+            progress:Number
+        }
+    ],
+    projects:[
+        {
+            title:String,
+            description:String,
+            github:String,
+            liveDemo:String
+        }
+    ],
+    certifications:[
+        {
+            title:String,
+            issure:String,
+            year:String,
+        }
+    ],
+    languages:[
+        {
+            name:String,
+            progress:Number
+        }
+    ],
+    interests:[String]
+},{
+    timestamps:{createdAt:'createdAt', updatedAt:'updateAt'}
+});
+
+module.exports = mongoose.model('Resume', resumeSchema);
